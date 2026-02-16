8c35b825af784daa66831662508828304ed2a7f4
JuanSalda91 <juansalda9131@gmail.com>
2026-02-15 23:18:26 -0500

    feat: get monthly revenue, get invoice stats, get low stock

---
+++ src/api/dashboardApi.js
@@ -0,0 +1,24 @@
+import axiosClient from "./axiosClient";
+
+/**
+ * dashboardApi
+ * 
+ * Small helper for endpoints used on the dashboard
+ * Uses axiosclient, so JWT is handled automatically
+ */
+export const dashboardApi = {
+    //GET /api/sales/revenue/monthly?year=yyy
+    getMonthlyRevenue: (year) => 
+        axiosClient.get('/sales/revenue/monthly', {
+            params: { year },
+        }),
+
+        // GET /api/invoices/stats
+        getinvoicesStats: () => axiosClient.get('/invoices/stats'),
+
+        // GET /api/products/low-stock?threshold=5
+        getLowStock: (threshold = 5) =>
+            axiosClient.get('/products/low-stock', {
+                params: { threshold },
+            }),
+};
