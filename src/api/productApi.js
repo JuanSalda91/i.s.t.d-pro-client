4b719a9deb9bc0406d8f6b10b0377d4c211e01c9
JuanSalda91 <juansalda9131@gmail.com>
2026-02-16 22:39:54 -0500

    feat: delete and update product added

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -1,8 +1,14 @@
 import axiosClient from './axiosClient.js';
 
 export const productApi = {
+
     createProduct: (data) => axiosClient.post('/products', data),
 
-    getProducts: (params) => 
-        axiosClient.get('/products', { params }),
+    getProducts: (params) => axiosClient.get('/products', { params }),
+
+    getProductById: (id) => axiosClient.get(`/products/${id}`),
+
+    deleteProduct: (id) => axiosClient.delete(`/products/${id}`),
+
+    updateProduct: (id, data) => axiosClient.put(`/products/${id}`, data),
 };
