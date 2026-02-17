b94fc6038f93c4f89465d424d6e403f3880d33c1
JuanSalda91 <juansalda9131@gmail.com>
2026-02-16 20:04:36 -0500

    update: login changed to fetch data properly

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,9 +1,8 @@
 import axiosClient from "./axiosClient";
 
 export const authApi = {
-    login: (email, password) => 
-        axiosClient.post('/auth/login', {email, password }),
+  login: (email, password) =>
+    axiosClient.post("/auth/login", { email, password }),
 
-    register: (name, email, password) =>
-        axiosClient.post('/auth/register', { name, email, password },)
+  register: (data) => axiosClient.post("/auth/register", data),
 };
